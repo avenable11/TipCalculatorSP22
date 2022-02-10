63da95196b0e7aac1e1a10681c7a7f27ac56e6f6
Angela Venable <avenable11@ivytech.edu>
2022-02-09 19:38:56 -0500

    ViewBinding Setup

--- app/src/main/java/edu/ivytech/tipcalculatorsp22/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculatorsp22/MainActivity.kt
@@ -2,10 +2,15 @@ package edu.ivytech.tipcalculatorsp22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.ivytech.tipcalculatorsp22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }

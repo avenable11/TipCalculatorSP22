7f46a2e0124519fdf8ef487a1e2fa53373731dc5
Angela Venable <avenable11@ivytech.edu>
2022-02-09 20:13:05 -0500

    Dropdown setup

--- app/src/main/java/edu/ivytech/tipcalculatorsp22/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculatorsp22/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.ivytech.tipcalculatorsp22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import edu.ivytech.tipcalculatorsp22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val items = resources.getStringArray(R.array.split_array)
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+        binding.splitBillDropdown.setAdapter(adapter)
+        binding.splitBillDropdown.setText(items[0], false)
     }
 }
